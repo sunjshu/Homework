fa1d48d9eca3c5f189d47a750aaddca8d5ce5e0e
Julia Nikolaeva <sunjshu@gmail.com>
2022-09-19 19:32:02 +0300

    CSharpSeminar6Homework

---
+++ Задача 41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите любое количество чисел разделенных запятой: ");
+string numbers = Console.ReadLine();
+string[] numbersSplit = numbers.Split(',');
+
+Console.WriteLine(CountPositiveNumbers(numbersSplit));
+
+int CountPositiveNumbers(string[] numSplit)
+{
+    int count = 0;
+    for (int i = 0; i < numSplit.Length; i++)
+    {
+        if (int.Parse(numSplit[i]) > 0)
+            count++;
+    }
+    return count;
+}
---
+++ Задача 43/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//  y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите b1: ");
+float b1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+float k1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+float b2 = float.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+float k2 = float.Parse(Console.ReadLine());
+
+PrintInterSection(b1, b2, k1, k2);
+
+void PrintInterSection(float b11, float b22, float k11, float k22)
+{
+    float x = (b22 - b11) / (k11 - k22);
+    Console.WriteLine($"Точка пересечения: ({x};{k11 * x + b11})");
+}
+
